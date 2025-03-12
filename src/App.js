1208d682a37cbb939f6163104469652e8071a697
Krishnakumar3k <krishnakumar3gt@gmail.com>
2025-03-12 12:46:29 +0530

    new Upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import SearchImg from './components/SearchImg';
 import CanvasImg from './components/CanvasImg';
 import './App.css';
-import Logo from './images/logo.png';
+// import Logo from '';
 
 function App() {
   const [selectedImage, setSelectedImage] = useState('');
@@ -13,15 +13,15 @@ function App() {
       <nav className="navbar bg-gray-800 p-4 text-white">
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center space-x-2">
-            <img src={Logo} alt="Logo" className="w-8 h-8" />
+            {/* <img src={Logo} alt="Logo" className="w-8 h-8" /> */}
             <h1 className="text-xl font-bold">Image Search App</h1>
           </div>
 
           {/* Desktop Menu */}
           <ul className="hidden md:flex space-x-4">
             <li><a href="https://sdekrishna.netlify.app/" className="hover:text-blue-400">Krishna Kumar</a></li>
-            <li>|</li>
-            <li><a href="mailto:krishnakumar3kgt@gmail.com" className="hover:text-blue-400">Email: krishnakumar3kgt@gmail.com</a></li>
+            {/* <li>|</li>
+            <li><a href="mailto:" className="hover:text-blue-400">Email:</a></li> */}
           </ul>
 
           {/* Mobile Menu Toggle Button */}
